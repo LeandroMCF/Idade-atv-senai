a8729d83f3ecd6a6476177fcfadd9eac11983168
DEV <dev@info.local>
2020-11-13 09:32:27 -0300

    analisando idade

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Idade_atv_senai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int mes;
+            int dia;
+            int horas;
+            int min;
+
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+            mes = idade * 12;
+            dia = 365 * idade;
+            horas = 8760 * idade;
+            min = 525600 * idade;
+            Console.WriteLine("Você tem "+mes+" meses");
+            Console.WriteLine("Você tem "+dia+" dias");
+            Console.WriteLine("Você tem "+horas+" horas");
+            Console.WriteLine("Você tem "+min+" minutos");
+        }
+    }
+}
